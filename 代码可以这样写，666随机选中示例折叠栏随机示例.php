6491f009d4f1f1f222c46e9e8daa2143d2fe3a5f
km3xy <77920834+km3xy@users.noreply.github.com>
2023-11-24 07:59:19 +0800

    Create 代码可以这样写，666随机选中示例折叠栏随机示例.php

---
+++ 代码可以这样写，666随机选中示例折叠栏随机示例.php
@@ -0,0 +1,451 @@
+//随机比较选中input  
+
+//
+
+
+<label>
+     
+  <input type="checkbox" 
+  
+<?php 
+
+$x=mt_rand(); 
+
+$e=mt_rand();
+
+if ($x>$e)
+{
+
+    echo  "checked";
+}
+else
+{
+
+    
+}
+
+  
+  
+?>
+ 
+  
+  name="c[666666]" id="c" value="666666">666666
+
+ </label>
+
+
+//折叠栏随机代码示例
+
+
+
+  <b ><h4>折叠栏1: 
+  
+
+ <input list="browsers" style="font-size:30px;color:#FF0000" type="text" name="a_ng[]" id="a1" value=""        ng-model="firstName">   
+
+
+
+<datalist id="browsers">
+ 
+ <option value="1+">                 
+ 
+ <?php  echo  mt_rand();?> 
+  
+<option value="2+">
+    
+ <?php  echo  mt_rand();?> 
+    
+    
+  
+<option value="3+">
+  
+  
+ <?php  echo  mt_rand();?> 
+  
+  
+  
+  
+<option value="4+">
+    
+    
+ <?php  echo  mt_rand();?> 
+  
+    
+    
+ 
+ <option value="5+">
+     
+     
+ <?php  echo  mt_rand();?> 
+  
+     
+     
+  
+<option value="6+">
+
+
+
+ <?php  echo  mt_rand();?> 
+  
+
+
+
+  <option value="7+">
+
+
+
+ <?php  echo  mt_rand();?> 
+  
+
+
+
+
+  <option value="8+">
+      
+      
+      
+ <?php  echo  mt_rand();?> 
+  
+      
+      
+      
+
+  <option value="9+">
+
+
+
+ <?php  echo  mt_rand();?> 
+  
+
+
+
+  <option value="10+">
+
+
+
+ <?php  echo  mt_rand();?> 
+  
+
+
+
+  <option value="11+">
+      
+      
+      
+ <?php  echo  mt_rand();?> 
+  
+      
+      
+
+  <option value="12+">
+ 
+ 
+ 
+ <?php  echo  mt_rand();?> 
+  
+ 
+ 
+ 
+  <option value="13+">
+      
+      
+      
+ <?php  echo  mt_rand();?> 
+  
+      
+
+  <option value="14+">
+
+
+
+ <?php  echo  mt_rand();?> 
+  
+
+
+  <option value="15+">
+
+
+ <?php  echo  mt_rand();?> 
+  
+
+
+  <option value="16+">
+      
+      
+ <?php  echo  mt_rand();?> 
+  
+      
+
+  <option value="17+">
+      
+      
+ <?php  echo  mt_rand();?> 
+  
+      
+      
+
+  <option value="18+">
+
+
+
+ <?php  echo  mt_rand();?> 
+  
+
+
+  <option value="19+">
+      
+      
+ <?php  echo  mt_rand();?> 
+  
+      
+      
+
+  <option value="20+">
+      
+      
+ <?php  echo  mt_rand();?> 
+  
+      
+
+  <option value="21+">
+      
+      
+      
+ <?php  echo  mt_rand();?> 
+  
+      
+
+  <option value="22+">
+
+
+
+ <?php  echo  mt_rand();?> 
+  
+
+
+  <option value="23+">
+      
+      
+      
+ <?php  echo  mt_rand();?> 
+  
+      
+
+  <option value="24+">
+
+
+
+
+
+  <option value="25+">
+      
+      
+      
+ <?php  echo  mt_rand();?> 
+  
+      
+
+  <option value="26+">
+
+
+
+ <?php  echo  mt_rand();?> 
+  
+
+
+  <option value="27+">
+      
+      
+ <?php  echo  mt_rand();?> 
+  
+      
+      
+
+  <option value="28+">
+
+
+ <?php  echo  mt_rand();?> 
+  
+
+
+  <option value="29+">
+
+
+
+ <?php  echo  mt_rand();?> 
+  
+
+  <option value="30+">
+      
+      
+      
+ <?php  echo  mt_rand();?> 
+  
+      
+
+  <option value="31+">
+
+
+
+ <?php  echo  mt_rand();?> 
+  
+
+
+  <option value="32+">
+      
+      
+ <?php  echo  mt_rand();?> 
+  
+      
+      
+
+  <option value="33+">
+
+
+
+ <?php  echo  mt_rand();?> 
+  
+
+
+  <option value="34+">
+      
+      
+      
+ <?php  echo  mt_rand();?> 
+  
+      
+
+  <option value="35+">
+      
+      
+      
+ <?php  echo  mt_rand();?> 
+  
+      
+
+</datalist>
+
+
+
+</b>
+
+
+
+
+
+
+        <label for="fullname"><h1>折叠栏代码示例：</label>
+       
+</p>
+
+
+
+
+ <b>折叠栏2 : 
+    
+    
+    
+<input  list="browserst2" style="font-size:30px;color:#FF0000" type="text" name="b_ng[]" id="b1" value=""      
+
+ng-model="lag">
+
+
+
+<datalist id="browserst2">
+ 
+ <option value="1+">
+  
+  
+ <?php  echo  mt_rand();?> 
+  
+  
+<option value="2+">
+    
+    
+ <?php  echo  mt_rand();?> 
+  
+  
+<option value="3+">
+    
+    
+ <?php  echo  mt_rand();?> 
+  
+  
+<option value="4+">
+    
+    
+<?php  echo  mt_rand();?> 
+    
+ 
+ <option value="5+">
+     
+     
+ <?php  echo  mt_rand();?> 
+     
+  
+<option value="6+">
+    
+    
+ <?php  echo  mt_rand();?> 
+
+  <option value="7+">
+      
+      
+ <?php  echo  mt_rand();?> 
+      
+
+  <option value="8+">
+
+
+ <?php  echo  mt_rand();?> 
+
+
+  <option value="9+">
+
+
+
+ <?php  echo  mt_rand();?> 
+
+
+
+  <option value="10+">
+
+
+
+ <?php  echo  mt_rand();?> 
+
+
+  <option value="11+">
+
+
+
+ <?php  echo  mt_rand();?> 
+
+  <option value="12+">
+ 
+ 
+ 
+ <?php  echo  mt_rand();?> 
+ 
+  
+
+</datalist>
+
+
+
+</b>
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
